7ca8e5e9ac136d12261d8b0a6265962fc4bc7baf
Laura Laane <laura.laane@gmail.com>
2020-11-09 11:56:53 +0200

    using list for booklist

--- src/index.js
+++ src/index.js
@@ -2,40 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-//setup variables
-const firstBook = {
-	img: 'https://images-na.ssl-images-amazon.com/images/I/91pR9wKJ3zL._AC_UL200_SR200,200_.jpg',
-	title: 'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
-	author: 'James Clear',
-};
-const secondBook = {
-	img: 'https://images-na.ssl-images-amazon.com/images/I/71A0CJuGSeL._AC_UL200_SR200,200_.jpg',
-	title: 'Burn After Writing',
-	author: 'Sharon Jones',
-};
+const books = [
+	{
+		id: 1,
+		img: 'https://images-na.ssl-images-amazon.com/images/I/91pR9wKJ3zL._AC_UL200_SR200,200_.jpg',
+		title: 'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
+		author: 'James Clear',
+	},
+	{
+		id: 2,
+		img: 'https://images-na.ssl-images-amazon.com/images/I/71A0CJuGSeL._AC_UL200_SR200,200_.jpg',
+		title: 'Burn After Writing',
+		author: 'Sharon Jones',
+	},
+	{
+		id: 3,
+		img: 'https://images-na.ssl-images-amazon.com/images/I/91AQs6qv9ML._AC_UL200_SR200,200_.jpg',
+		title: 'Untamed',
+		author: 'Glennon Doyle',
+	},
+];
 
 function Booklist() {
 	return (
 		<section className='booklist'>
-			<Book img={firstBook.img} title={firstBook.title} author={firstBook.author}>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente odit excepturi cupiditate dolor vitae saepe
-					tempora voluptatem itaque exercitationem? Vero, cum ratione in optio nesciunt ipsa et veritatis enim
-					doloribus.
-				</p>
-			</Book>
-			<Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
+			{books.map((book) => {
+				return <Book key={book.id} book={book}></Book>;
+			})}
 		</section>
 	);
 }
 const Book = (props) => {
 	// const { img, title, author } = props; // kui nii kirjutada ss ei pea props allpool kgg ette kirjutama et valuesid saada
+	const { img, title, author } = props.book; //props.book peab kirjutama sest praegu ylemine map tagastab objecti
 	return (
 		<article className='book'>
-			<img src={props.img} alt='bookcase' />
-			<h1>{props.title}</h1>
-			<h4>{props.author}</h4>
-			{props.children} {/*children on special prop, siin kasutame selleks et booki p sisu lehel n2ha*/}
+			<img src={img} alt='bookcase' />
+			<h1>{title}</h1>
+			<h4>{author}</h4>
+			{/* {props.children} children on special prop, siin kasutasime selleks et book tagide vahel olevat sisu kuvataks*/}
 		</article>
 	);
 };
